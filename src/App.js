8958a0b1fe9711fbff41cef69efeeb79a9216f7d
TQT67 <trangtrinh000000@gmail.com>
2023-02-17 20:31:31 +0700

    th2

--- src/App.js
+++ src/App.js
@@ -1,25 +1,33 @@
-import logo from './logo.svg';
-import './App.css';
+import { createBrowserRouter, Link, RouterProvider } from "react-router-dom";
+import Layout from "./Layout";
+
+const router = createBrowserRouter([
+  {
+    path: "/",
+    element: <Layout />,
+    children: [
+      {
+        path: "/about",
+        element: <div>About Us</div>,
+      },
+      {
+        path: "/about/us",
+        element: <div>US</div>,
+      },
+      {
+        path: "/about/me",
+        element: <div>me</div>,
+      },
+    ],
+  },
+  {
+    path: "*",
+    element: <div>404-Not Found</div>,
+  },
+]);
 
 function App() {
-  return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
-  );
+  return <RouterProvider router={router} />;
 }
 
 export default App;
